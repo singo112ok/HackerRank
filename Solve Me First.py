7fcabfc29c8ff0aa0f2401584c5b453edfff0c5d
Woo YongHyeon <singo112ok@nate.com>
2020-07-31 00:28:25 +0900

    solvemeFirst

---
+++ Solve Me First.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/solve-me-first/problem
+
+# 처음써본 파이썬, 해커랭크 시작
+
+def solveMeFirst(a,b):
+    return num1+num2
+
+if __name__ == '__main__':
+    num1 = input()
+    num2 = input()
+    res = solveMeFirst(num1,num2)
+    print res
