7d145a2918149f79fb41c46b0c5b0ece65976846
yonghyeon-woo <singo112ok@gmailcom>
2020-10-13 21:54:21 +0900

    통과는 했었으나 풀이를 잘못해서 다시 푼 문제
    오름차순으로 정렬 후 현재위치와 이전위치 값 비교
    같으면 둘다 삭제 후 다시 처음부터 비교
    계속 지우다보면 결국 길이가 1이 되고 해당값 출력
    ----------------------------------------
    문제 탭을 안읽어서 string 식으로 풀었다..
    비트 조작답게 값들을 xor하면 동일값은 사라지게되어
    유일한 값만 남게된다

--- Bit Manipulation/Lonely Integer.py
+++ Bit Manipulation/Lonely Integer.py
@@ -1,5 +1,12 @@
 # https://www.hackerrank.com/challenges/lonely-integer/problem
 
+# 오름차순으로 정렬 후 현재위치와 이전위치 값 비교
+# 같으면 둘다 삭제 후 다시 처음부터 비교
+# 계속 지우다보면 결국 길이가 1이 되고 해당값 출력
+# ----------------------------------------
+
+
+
 #!/bin/python3
 
 import math
@@ -9,22 +16,31 @@ import re
 import sys
 
 # Complete the lonelyinteger function below.
+# def lonelyinteger_old(a):
+#     a.sort()
+#     nIndex = 0
+
+#     while True:
+#         nIndex += 1
+
+#         nLenA = len(a) 
+#         if nLenA == 1:
+#             return a[0] 
+
+#         if a[nIndex] == a[nIndex-1]:
+#             del a[nIndex]
+#             del a[nIndex-1]
+#             nIndex = 0
+#             continue        
+
 def lonelyinteger(a):
-    a.sort()
-    nIndex = 0
+    result = 0
 
-    while True:
-        nIndex += 1
+    for i in a:
+        result = result ^ i
+    return result
 
-        nLenA = len(a) 
-        if nLenA == 1:
-            return a[0] 
 
-        if a[nIndex] == a[nIndex-1]:
-            del a[nIndex]
-            del a[nIndex-1]
-            nIndex = 0
-            continue        
         
 
 if __name__ == '__main__':
